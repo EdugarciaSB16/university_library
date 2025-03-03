af4a9cd90e5b4a0c821d479e290ab319ca212d04
Eduardo Garcia <edugarciasb16@gmail.com>
2025-03-03 03:17:25 +0100

    feat: add new structure files

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import BookList from '@/components/BookList';
+import BookOverview from '@/components/BookOverview';
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const Home = () => (
+    <>
+        <BookOverview />
+        <BookList />
+    </>
+);
+
+export default Home;
