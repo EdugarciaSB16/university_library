9490875c81993a6a7d4da5fbbb74a2cc9e324360
Eduardo Garcia <edugarciasb16@gmail.com>
2025-03-04 04:10:16 +0100

    feat: add Book interface for type definition

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface Book {
+  id: number;
+  title: string;
+  author: string;
+  genre: string;
+  rating: number;
+  total_copies: number;
+  available_copies: number;
+  description: string;
+  coverColor: string;
+  coverUrl: string;
+  video: string;
+  isLoanedBook?: boolean;
+}
