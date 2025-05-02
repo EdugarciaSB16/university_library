fa838b9981e0108f3c92f75a4e4da9603d12a93c
Eduardo Garcia <edugarciasb16@gmail.com>
2025-05-02 02:38:18 +0200

    refactor url import

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,22 @@
 export interface Book {
-  id: number;
-  title: string;
-  author: string;
-  genre: string;
-  rating: number;
-  totalCopies: number;
-  availableCopies: number;
-  description: string;
-  coverColor: string;
-  coverUrl: string;
-  videoUrl: string;
-  isLoanedBook?: boolean;
+    id: number;
+    title: string;
+    author: string;
+    genre: string;
+    rating: number;
+    totalCopies: number;
+    availableCopies: number;
+    description: string;
+    coverColor: string;
+    coverUrl: string;
+    videoUrl: string;
+    isLoanedBook?: boolean;
+}
+
+export interface AuthCredentials {
+    fullName: string;
+    email: string;
+    password: string;
+    universityId: number;
+    universityCard: string;
 }
