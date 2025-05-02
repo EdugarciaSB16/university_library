dd86e9440d91c339d7327f89ef7a99150a31d9f3
Eduardo Garcia <edugarciasb16@gmail.com>
2025-05-02 02:39:38 +0200

    feat(auth): implement authentication library and setup basic config

---
+++ src/lib/actions/auth.ts
@@ -0,0 +1,65 @@
+"use server";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { signIn } from "@/lib/auth";
+import { hash } from "bcryptjs";
+import { eq } from "drizzle-orm";
+
+export const signInWithCredentials = async (
+  params: Pick<AuthCredentials, "email" | "password">,
+) => {
+  const { email, password } = params;
+
+  try {
+    const result = await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+    });
+
+    if (result?.error) {
+      return { success: false, error: result.error };
+    }
+
+    return { success: true };
+  } catch (error) {
+    console.error(error, "SignIn error");
+    return { success: false, error: "Failed to sign in" };
+  }
+};
+
+export const signUp = async (params: AuthCredentials) => {
+  const { fullName, email, password, universityId, universityCard } = params;
+
+  // Check if the user already exits
+  const existingUser = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email))
+    .limit(1);
+
+  if (existingUser.length > 0) {
+    return { success: false, error: "User already exists" };
+  }
+
+  const hashedPassword = await hash(password, 10);
+
+  try {
+    await db.insert(users).values({
+      fullName,
+      email,
+      universityId,
+      password: hashedPassword,
+      universityCard,
+    });
+
+    await signInWithCredentials({
+      email,
+      password,
+    });
+    return { success: true };
+  } catch (error) {
+    console.error(error, "SignUp error");
+    return { success: false, error: "Failed to create user" };
+  }
+};
