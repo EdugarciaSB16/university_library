94aad21fc1b1075bf21360d23b96194f0f4e1c3c
Eduardo Garcia <edugarciasb16@gmail.com>
2025-05-04 19:55:51 +0200

    feat: add route structure for All books

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default page;
