3c39b2a494664324af9d4abaefa5b42fd8fa2ae6
Eduardo Garcia <edugarciasb16@gmail.com>
2025-05-05 03:05:35 +0200

    feat: add BorrowBook client component with borrowing logic and toast feedback

--- src/components/BookOverview.tsx
+++ src/components/BookOverview.tsx
@@ -1,12 +1,16 @@
 import { Book } from '@/types';
 import Image from 'next/image';
 import BookCover from './BookCover';
-import { Button } from './ui/button';
+import BorrowBook from './BorrowBook';
+import { db } from '@/database/drizzle';
+import { users } from '@/database/schema';
+import { eq } from 'drizzle-orm';
+import Link from 'next/link';
 interface Props extends Book {
   userId: string;
 }
 
-const BookOverview: React.FC<Props> = ({
+const BookOverview = async ({
   title,
   author,
   genre,
@@ -19,45 +23,71 @@ const BookOverview: React.FC<Props> = ({
   id,
   userId,
 }: Props) => {
+  const [user] = await db
+    .select()
+    .from(users)
+    .where(eq(users.id, userId))
+    .limit(1);
+
+  const borrowingEligibility = {
+    isEligible: availableCopies > 0 && user?.status === 'APPROVED',
+    message:
+      availableCopies <= 0
+        ? 'Book is not available'
+        : 'You are not eligible to borrow this book',
+  };
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
         <h1>{title}</h1>
+
         <div className="book-info">
           <p>
             By <span className="font-semibold text-light-200">{author}</span>
           </p>
+
           <p>
             Category{' '}
             <span className="font-semibold text-light-200">{genre}</span>
           </p>
+
           <div className="flex flex-row gap-1">
             <Image src="/icons/star.svg" alt="star" width={22} height={22} />
             <p>{rating}</p>
           </div>
         </div>
+
         <div className="book-copies">
           <p>
-            Total Books: <span>{totalCopies}</span>
+            Total Books <span>{totalCopies}</span>
           </p>
+
           <p>
-            Available Books: <span>{availableCopies}</span>
+            Available Books <span>{availableCopies}</span>
           </p>
-          <p className="book-description">{description}</p>
-          <Button className="book-overview_btn">
-            <Image src="/icons/book.svg" alt="book" width={20} height={20} />
-            <p className="font-bebas-neue text-xl text-dark-100">Borrow</p>
-          </Button>
         </div>
+
+        <p className="book-description">{description}</p>
+
+        {user && (
+          <BorrowBook
+            bookId={id}
+            userId={userId}
+            borrowingEligibility={borrowingEligibility}
+          />
+        )}
       </div>
+
       <div className="relative flex flex-1 justify-center">
         <div className="relative">
-          <BookCover
-            variant="wide"
-            className="z-10"
-            coverColor={coverColor}
-            coverImage={coverUrl}
-          />
+          <Link href={`/books/${id}`} className="z-10">
+            <BookCover
+              variant="wide"
+              className="z-10"
+              coverColor={coverColor}
+              coverImage={coverUrl}
+            />
+          </Link>
           <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
             <BookCover
               variant="wide"
