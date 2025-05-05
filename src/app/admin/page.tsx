2ed9c46e6d51fd2b1fabd23fc56c39e7eb7e39f2
Eduardo Garcia <edugarciasb16@gmail.com>
2025-05-05 03:04:50 +0200

    feat: add onboarding admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,28 @@
+'use client';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import React from 'react';
 
 const page = () => {
   return (
-    <div>
-      <div></div>
+    <div className="p-10 space-y-6">
+      <div>
+        <h1 className="text-3xl font-bold text-dark-400">
+          Welcome to the Admin Dashboard
+        </h1>
+        <p className="mt-2 text-base text-slate-600">
+          From here, you can manage your university library. Right now, you can
+          create a new book entry to make it available to students.
+        </p>
+      </div>
+
+      <div>
+        <Link href="/admin/books/new">
+          <Button className="bg-primary-admin text-white hover:bg-opacity-80">
+            Create New Book
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
